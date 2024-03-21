16592dfe907f4bdd00ebfc1e4121555edb8c1981
Bell_Wong <wtpbell@gmail.com>
2024-03-21 19:54:15 +0100

    try development mode

--- starting-code/webpack.config.js
+++ starting-code/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./code/main.js",
   module: {
     rules: [
