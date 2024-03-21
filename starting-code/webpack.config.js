ab5743eb9ae097002f22358d40f06bb5238eefd7
Bell_Wong <wtpbell@gmail.com>
2024-03-21 19:27:31 +0100

    formating

---
+++ starting-code/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: "./code/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ttf$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.png$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.txt$/i,
+        type: "asset/source",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })]
+};
