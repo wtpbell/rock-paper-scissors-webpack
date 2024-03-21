adfdd80680f5ef397baa0c6bea1245cd0fa24be8
Bell_Wong <wtpbell@gmail.com>
2024-03-21 19:36:31 +0100

    change to production mode

--- starting-code/webpack.config.js
+++ starting-code/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./code/main.js",
   module: {
     rules: [
